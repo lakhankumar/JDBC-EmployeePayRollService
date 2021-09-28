a9f3c78228c8aab695b6e80bdfd71f0a40364faf
lakhankumar <lakhankumar997@gmail.com>
2021-09-28 23:06:42 +0530

    load drivers and establish connection to database

--- main/java/EmployeePayRoll.java
+++ main/java/EmployeePayRoll.java
@@ -1,5 +1,6 @@
 public class EmployeePayRoll {
     public static void main(String[] args) {
-        System.out.println("welcome to Employee pay roll service");
+        EmployeeConfig eConfig = new EmployeeConfig();
+        eConfig.getConfig();
     }
 }
